68eb0f7ffe4609408538e3939c94120e20d413ae
liangfutang <liangfu.tang@ecarx.com.cn>
2019-08-01 22:14:42 +0800

    添加日志追踪的traceID的测试

--- boot/src/main/java/com/zjut/dubbo/consumer/controller/TestController.java
+++ boot/src/main/java/com/zjut/dubbo/consumer/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.zjut.dubbo.consumer.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.zjut.dubbo.consumer.response.RestResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/test")
@@ -17,5 +21,17 @@ public class TestController {
         logger.info("日志测试");
         System.out.println("进入到测试方法");
         return new RestResponse("test ssuccess");
+    }
+
+    @GetMapping("/testThreadPool")
+    public RestResponse testThreadPool() throws InterruptedException {
+        RestTemplate restTemplate = new RestTemplate();
+        for (int i=0; i<10; i++) {
+            ResponseEntity<JSONObject> forEntity = restTemplate.getForEntity("http://localhost:8080/test/testThreadPool", JSONObject.class);
+            Thread.sleep(3000);
+        }
+
+
+        return new RestResponse("success");
     }
 }
