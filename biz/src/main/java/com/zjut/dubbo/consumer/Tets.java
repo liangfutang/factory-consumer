1b0613a0c459325119c82224e4171412ad380f56
liangfutang <liangfu.tang@ecarx.com.cn>
2019-09-23 19:13:40 +0800

    添加数据库的依赖，并使用starter中的traceID

---
+++ biz/src/main/java/com/zjut/dubbo/consumer/Tets.java
@@ -0,0 +1,7 @@
+package com.zjut.dubbo.consumer;
+
+/*
+* 站位
+* */
+public class Tets {
+}
--- config/src/main/java/com/zjut/dubbo/consumer/config/filters/FilterConfig.java
+++ config/src/main/java/com/zjut/dubbo/consumer/config/filters/FilterConfig.java
@@ -1,20 +1,20 @@
-package com.zjut.dubbo.consumer.config.filters;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FilterConfig {
-
-    @Bean
-    public FilterRegistrationBean setTraceIdFilter() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new TraceIdFilter());
-        registration.addUrlPatterns("/*");
-        registration.setName("traceIdFilter");
-        registration.setOrder(1);
-        return registration;
-    }
-
-}
+//package com.zjut.dubbo.consumer.config.filters;
+//
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class FilterConfig {
+//
+//    @Bean
+//    public FilterRegistrationBean setTraceIdFilter() {
+//        FilterRegistrationBean registration = new FilterRegistrationBean();
+//        registration.setFilter(new TraceIdFilter());
+//        registration.addUrlPatterns("/*");
+//        registration.setName("traceIdFilter");
+//        registration.setOrder(1);
+//        return registration;
+//    }
+//
+//}
