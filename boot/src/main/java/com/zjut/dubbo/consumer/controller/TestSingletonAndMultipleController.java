1f618f6cb838afaa8093e27fae5038f15ae9e56c
liangfutang <liangfu.tang@ecarx.com.cn>
2019-11-28 20:51:23 +0800

    添加单例和多例的测试

---
+++ boot/src/main/java/com/zjut/dubbo/consumer/controller/TestSingletonAndMultipleController.java
@@ -0,0 +1,27 @@
+package com.zjut.dubbo.consumer.controller;
+
+import com.zjut.dubbo.consumer.common.response.RestResponse;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/instance")
+//@Scope("prototype") //默认的情况下是单例，加上该注解就是多例了
+public class TestSingletonAndMultipleController {
+
+    private int i;
+
+    /**
+     * 测试全局变量是否会增加，单例的话会，多例的话不会
+     * 返回的结果是会变化
+     * @return
+     */
+    @PutMapping("/test1")
+    public RestResponse getI1() {
+        i++;
+        return new RestResponse(i);
+    }
+
+}
