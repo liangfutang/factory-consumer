327f4addba8603cda19962b53a27981f3c4dbde5
liangfutang <liangfu.tang@ecarx.com.cn>
2020-01-03 11:10:11 +0800

    provider修改了文件夹名，所以，此处修改引用的文件夹名

--- call/src/main/java/com/zjut/dubbo/consumer/call/impl/TestDubboServiceImpl.java
+++ call/src/main/java/com/zjut/dubbo/consumer/call/impl/TestDubboServiceImpl.java
@@ -2,8 +2,8 @@ package com.zjut.dubbo.consumer.call.impl;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.zjut.dubbo.consumer.call.TestDubboService;
-import com.zjut.dubbo.provider.client.dto.TestDubboValidationDto;
-import com.zjut.dubbo.provider.client.service.TestService;
+import com.zjut.factory.provider.client.dto.TestDubboValidationDto;
+import com.zjut.factory.provider.client.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
