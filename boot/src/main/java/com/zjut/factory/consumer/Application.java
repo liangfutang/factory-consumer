a94742286c5fc4e2b72466dd610d3e2ff3006844
liangfutang <liangfu.tang@ecarx.com.cn>
2020-09-12 01:13:01 +0800

    添加dubbo扫描

--- boot/src/main/java/com/zjut/factory/consumer/Application.java
+++ boot/src/main/java/com/zjut/factory/consumer/Application.java
@@ -1,11 +1,13 @@
 package com.zjut.factory.consumer;
 
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.zjut.dubbo.consumer.mapper")
+@DubboComponentScan(basePackages = {"com.zjut.factory.consumer.call.impl"})
 //@EnableTransactionManagement
 //@EnableAspectJAutoProxy(exposeProxy=true,proxyTargetClass=true)
 public class Application {
